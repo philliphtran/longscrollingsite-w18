2b24a2a67030aa642f132c17c7ebb4cee8d94a69
PhillipTran <ptran86@horizon.csueastbay.edu>
2018-03-06 15:59:37 -0800

    added boxes

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var counter = document.getElementById('counter');
 var fromTop = window.pageYOffset;
 var a = document.getElementById('a');
 var b = document.getElementById('b');
-
+var c = document.getElementById('c');
 
 
 counter.innerHTML = "Sinning";
@@ -27,6 +27,10 @@ var  y  = window.pageYOffset;
   counter.innerHTML = y + "px";
   a.style.top = 50 + y + "px";
   a.innerHTML = y + "px";
+  b.style.top = 150 + y + "px";
+  b.innerHTML = y + "px";
+  c.style.top = 150 + y + "px";
+  c.innerHTML = y + "px";
 //   if (fromTop > 5000){
 //     counter.innerHTML ="why me?";
 //     counter.style.backgroundColor= "red";
@@ -45,12 +49,16 @@ var  y  = window.pageYOffset;
     counter.style.backgroundColor = "clear";
     a.style.backgroundColor = "white";
     a.innerHTML ="<h1> 7 Deadly Sins </h1>";
+    b.style.backgroundColor = "black";
+    // b.innerHTML ="<h1> 7 Deadly Sins </h1>";
+    c.style.backgroundColor = "black";
+    c.innerHTML ="<h1> 7 Deadly Sins </h1>";
     counter.innerHTML = "Sinning";
     // pride.style.backgroundImage= "url()";
 
   }else if (y > 1000 && y < 2000) {
     console.log("y > 1000 && y < 2000 ");
-    a.innerHTML ="<h1> 1. Pride </h1>";
+    a.innerHTML ="<h2> 1. Pride </h2>";
     a.style.backgroundColor = "white";
     counter.style.backgroundColor = "purple";
     counter.innerHTML = "Ahhhh";
@@ -59,7 +67,7 @@ var  y  = window.pageYOffset;
   else if (y > 2000 && y < 3000 ) {
     console.log("y > 2000 && y < 3000 ");
     counter.style.backgroundColor = "red";
-    a.innerHTML ="<h1> 2. Gluttony </h1>";
+    a.innerHTML ="<h2> 2. Gluttony </h2>";
     a.style.backgroundColor = "white";
     counter.innerHTML = "Ahhhh";
   }
@@ -67,7 +75,7 @@ var  y  = window.pageYOffset;
     else if (y > 3000 && y < 4000 ) {
       console.log("y > 3000 && y < 4000 ");
       counter.style.backgroundColor = "yellow";
-      a.innerHTML ="<h1> 3. Sloth </h1>";
+      a.innerHTML ="<h2> 3. Sloth </h2>";
       a.style.backgroundColor = "white";
       counter.innerHTML = "Ahhhh";
     // a.innerHTML = " " //to change text
@@ -77,28 +85,28 @@ var  y  = window.pageYOffset;
   else if (y > 4000 && y < 5000 ) {
     console.log("y > 4000 && y < 5000 ");
     counter.style.backgroundColor = "red";
-    a.innerHTML ="<h1> 4. Greed </h1>";
+    a.innerHTML ="<h2> 4. Greed </h2>";
     a.style.backgroundColor = "white";
     counter.innerHTML = "Ahhhh";
   }
   else if (y > 5000 && y < 6000 ) {
     console.log("y > 5000 && y < 6000 ");
     counter.style.backgroundColor = "white";
-    a.innerHTML ="<h1> 5. Envy </h1>";
+    a.innerHTML ="<h2> 5. Envy </h2>";
     a.style.backgroundColor = "white";
     counter.innerHTML = "Ahhhh";
   }
   else if (y > 6000 && y < 7000 ) {
     console.log("y > 6000 && y < 7000 ");
     counter.style.backgroundColor = "cyan";
-    a.innerHTML ="<h1> 6. Anger </h1>";
+    a.innerHTML ="<h2> 6. Anger </h2>";
     a.style.backgroundColor = "white";
     counter.innerHTML = "Ahhhh";
   }
   else if (y > 7000 && y < 8000 ) {
     console.log("y > 7000 && y < 8000 ");
     counter.style.backgroundColor = "orange";
-    a.innerHTML ="<h1> 7. Envy </h1>";
+    a.innerHTML ="<h2> 7. Envy </h2>";
     a.style.backgroundColor = "white";
     counter.innerHTML = "Ahhhh";
   }
@@ -106,7 +114,8 @@ var  y  = window.pageYOffset;
     console.log("else");
     console.log("y > 8000");
     counter.style.backgroundColor = "green";
-    a.innerHTML ="<h1> The End </h1>";
+    a.innerHTML ="<h2> The End </h2>";
     a.style.backgroundColor = "white";
+    counter.innerHTML = "I am dead!";
   }
 }
